3ef7647dfcb01abfd100e897c90a277ccc99e37b
Steven <1938848188@qq.com>
2020-07-19 13:05:40 +0800

    添加js文件

---
+++ server.js
@@ -0,0 +1,58 @@
+var http = require("http");
+var fs = require("fs");
+var url = require("url");
+var port = process.argv[2];
+
+if (!port) {
+  console.log("请指定端口号好不啦？\nnode server.js 8888 这样不会吗？");
+  process.exit(1);
+}
+
+var server = http.createServer(function (request, response) {
+  var parsedUrl = url.parse(request.url, true);
+  var pathWithQuery = request.url;
+  var queryString = "";
+  if (pathWithQuery.indexOf("?") >= 0) {
+    queryString = pathWithQuery.substring(pathWithQuery.indexOf("?"));
+  }
+  var path = parsedUrl.pathname;
+  var query = parsedUrl.query;
+  var method = request.method;
+
+  /******** 从这里开始看，上面不要看 ************/
+
+  console.log("有个傻子发请求过来啦！路径（带查询参数）为：" + pathWithQuery);
+  if (path === "/") {
+    response.statusCode = 200;
+    response.setHeader("Content-Type", "text/html;charset=utf-8");
+    response.write(`
+      <link rel="stylesheet" href="./X">
+      <h1>你好</h1>
+      <script src="/Y"></script>
+    `);
+    response.end();
+  } else if (path === "/X") {
+    response.statusCode = 200;
+    response.setHeader("Content-Type", "text/css;charset=utf-8");
+    response.write(`h1{color: red;}`);
+    response.end();
+  } else if (path === "/Y") {
+    response.statusCode = 200;
+    response.setHeader("Content-Type", "text/javascript;charset=utf-8");
+    response.write(`console.log("这是JS内容")`);
+    response.end();
+  } else {
+    response.statusCode = 404;
+    response.end();
+  }
+
+  /******** 代码结束，下面不要看 ************/
+});
+
+server.listen(port);
+console.log(
+  "监听 " +
+    port +
+    " 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:" +
+    port
+);
